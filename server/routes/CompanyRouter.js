b3cafbe81e587fb20ab1136c335fd58564e4518d
ritikkr70 <ritikkr70@gmail.com>
2023-05-06 19:14:21 +0530

    frontend

--- server/routes/CompanyRouter.js
+++ server/routes/CompanyRouter.js
@@ -3,11 +3,10 @@ const router = express.Router();
 
 const Company = require('../models/Company');
 
-
+const registerCompany = require('../controller/CompanyController/CRegisterController');
 const updateCompanyInfo = require('../controller/CompanyController/CUpdateCompanyInfo');
 
-
-
+router.post('/register', registerCompany);
 router.post('/update', updateCompanyInfo);
 
 module.exports = router;
